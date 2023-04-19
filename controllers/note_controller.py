af6e9ea763e489d435c6c03e6dd4de8355d83b86
askAboutKevin <kdowdy1021@gmail.com>
2023-04-19 13:35:32 -0400

    only include boilerplate code

--- controllers/note_controller.py
+++ controllers/note_controller.py
@@ -12,35 +12,13 @@ def get_notes():
     return jsonify([vars(note) for note in notes])
 
 
-@note_blueprint.route("/notes/<int:note_id>", methods=["GET"])
-def get_note(note_id):
-    note = note_service.get_note_by_id(note_id)
-    if note:
-        return jsonify(vars(note))
-    else:
-        return "Note not found", 404
+# function get note by id
 
 
-@note_blueprint.route("/notes", methods=["POST"])
-def create_note():
-
-    # example error handling
-    if not request.json or 'title' not in request.json:
-        return jsonify({"error": "Bad request"}), 400
-
-    data = request.get_json()
-    note = Note(data["id"], data["title"], data["content"])
-    note_service.create_note(note)
-    return "Note created", 201
+# function create note
 
 
-@note_blueprint.route("/notes/<int:note_id>", methods=["PUT"])
-def update_note(note_id):
-    data = request.get_json()
-    note = Note(note_id, data["title"], data["content"])
-    note_service.update_note(note_id, note)
-    return "Note updated", 200
-
+# function update note
 
 @note_blueprint.route("/notes/<int:note_id>", methods=["DELETE"])
 def delete_note(note_id):
--- data_access/note_data_access.py
+++ data_access/note_data_access.py
@@ -3,7 +3,28 @@ import json
 
 DATA_FILE = "notes.json"
 
+# example of a note
+# {
+#     "id": 1,
+#     "title": "My first note",
+#     "content": "This is my first note"
+# }
 
+# example of a list of notes
+# [
+#     {
+#         "id": 1,
+#         "title": "My first note",
+#         "content": "This is my first note"
+#     },
+#     {
+#         "id": 2,
+#         "title": "My second note",
+#         "content": "This is my second note"
+#     }
+# ]
+
+### start helper functions to read and write notes ###
 def read_notes():
     try:
         with open(DATA_FILE, "r") as f:
@@ -15,30 +36,18 @@ def read_notes():
 def write_notes(notes):
     with open(DATA_FILE, "w") as f:
         json.dump(notes, f, indent=4)
+### end helper functions to read and write notes ###
 
 
 def get_notes():
     return read_notes()
 
 
-def get_note_by_id(note_id):
-    notes = read_notes()
-    return next((note for note in notes if note["id"] == note_id), None)
+# def get_note_by_id(note_id):
 
+# def create_note(note):
 
-def create_note(note):
-    notes = read_notes()
-    notes.append(vars(note))
-    write_notes(notes)
-
-
-def update_note(note_id, note):
-    notes = read_notes()
-    index = next((i for i, n in enumerate(notes) if n["id"] == note_id), None)
-    if index is not None:
-        notes[index] = vars(note)
-        write_notes(notes)
-
+# def update_note(note_id, note):
 
 def delete_note(note_id):
     notes = read_notes()
